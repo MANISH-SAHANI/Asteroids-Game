e7dcac8e74d65e9511d030f31e9bce6541236d66
Manish Sahani <91081774+MANISH-SAHANI@users.noreply.github.com>
2023-07-20 20:46:52 +0530

    Game Files

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Bullet : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public float speed = 500f;
+    public float maxLifetime = 10f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Project(Vector2 direction)
+    {
+        // The bullet only needs a force to be added once since they have no
+        // drag to make them stop moving
+        rigidbody.AddForce(direction * speed);
+
+        // Destroy the bullet after it reaches it max lifetime
+        Destroy(gameObject, maxLifetime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Destroy the bullet as soon as it collides with anything
+        Destroy(gameObject);
+    }
+
+}
